4dd054df2796a8499b98bef1f779e012c3aab005
jmjlacosta <jmjlacosta@gmail.com>
2024-11-23 14:29:27 -0600

    optional fields for FastAPI using typing

--- eLVEF_fastapi.py
+++ eLVEF_fastapi.py
@@ -10,36 +10,36 @@ app = FastAPI(
 )
 
 class ProbabilityInput(BaseModel):
-    male: Optional[bool] 
-    age: int = Field(..., ge=0, le=120, description="Age of the patient")
-    dx_defibrillator: Optional[bool]
-    hosp_chf: Optional[bool]
-    rx_ace: Optional[bool]
-    rx_antagonist: Optional[bool]
-    rx_bblocker: Optional[bool]
-    rx_digoxin: Optional[bool]
-    rx_loop_diuretic: Optional[bool]
-    rx_nitrates: Optional[bool]
-    rx_thiazide: Optional[bool]
-    dx_afib: Optional[bool]
-    dx_anemia: Optional[bool]
-    dx_cabg: Optional[bool]
-    dx_cardiomyopathy: Optional[bool]
-    dx_copd: Optional[bool]
-    dx_depression: Optional[bool]
-    dx_htn_nephropathy: Optional[bool]
-    dx_hyperlipidemia: Optional[bool]
-    dx_hypertension: Optional[bool]
-    dx_hypotension: Optional[bool]
-    dx_mi: Optional[bool]
-    dx_obesity: Optional[bool]
-    dx_oth_dysrhythmia: Optional[bool]
-    dx_psychosis: Optional[bool]
-    dx_rheumatic_heart: Optional[bool]
-    dx_sleep_apnea: Optional[bool]
-    dx_stable_angina: Optional[bool]
-    dx_valve_disorder: Optional[bool]
-    hf_type: Optional[Literal["Systolic", "Diastolic", "Left", "Unspecified"]]
+    male: Optional[bool] = None
+    age: Optional[int] = Field(None, ge=0, le=120, description="Age of the patient")
+    dx_defibrillator: Optional[bool] = None
+    hosp_chf: Optional[bool] = None
+    rx_ace: Optional[bool] = None
+    rx_antagonist: Optional[bool] = None
+    rx_bblocker: Optional[bool] = None
+    rx_digoxin: Optional[bool] = None
+    rx_loop_diuretic: Optional[bool] = None
+    rx_nitrates: Optional[bool] = None
+    rx_thiazide: Optional[bool] = None
+    dx_afib: Optional[bool] = None
+    dx_anemia: Optional[bool] = None
+    dx_cabg: Optional[bool] = None
+    dx_cardiomyopathy: Optional[bool] = None
+    dx_copd: Optional[bool] = None
+    dx_depression: Optional[bool] = None
+    dx_htn_nephropathy: Optional[bool] = None
+    dx_hyperlipidemia: Optional[bool] = None
+    dx_hypertension: Optional[bool] = None
+    dx_hypotension: Optional[bool] = None
+    dx_mi: Optional[bool] = None
+    dx_obesity: Optional[bool] = None
+    dx_oth_dysrhythmia: Optional[bool] = None
+    dx_psychosis: Optional[bool] = None
+    dx_rheumatic_heart: Optional[bool] = None
+    dx_sleep_apnea: Optional[bool] = None
+    dx_stable_angina: Optional[bool] = None
+    dx_valve_disorder: Optional[bool] = None
+    hf_type: Optional[Literal["Systolic", "Diastolic", "Left", "Unspecified"]] = None
 
 class ProbabilityOutput(BaseModel):
     probability: float
@@ -50,37 +50,37 @@ def calculate_probability(data: ProbabilityInput) -> ProbabilityOutput:
     intercept = -1.37218706653502
     LP0 = intercept
 
-    # Linear predictor calculations based on input values
-    LP0 += 0.323651 * data.male
+    # Handle None values by defaulting to 0 in calculations
+    LP0 += 0.323651 * (data.male or 0)
     LP0 += -0.187191
-    LP0 += -0.005747 * data.age
-    LP0 += 0.275032 * data.dx_defibrillator
-    LP0 += 0.346289 * data.hosp_chf
-    LP0 += 0.221748 * data.rx_ace
-    LP0 += 0.166008 * data.rx_antagonist
-    LP0 += 0.087257 * data.rx_bblocker
-    LP0 += 0.163224 * data.rx_digoxin
-    LP0 += 0.084251 * data.rx_loop_diuretic
-    LP0 += 0.129225 * data.rx_nitrates
-    LP0 += -0.160819 * data.rx_thiazide
-    LP0 += -0.002267 * data.dx_afib
-    LP0 += -0.165353 * data.dx_anemia
-    LP0 += -0.040175 * data.dx_cabg
-    LP0 += 1.415113 * data.dx_cardiomyopathy
-    LP0 += -0.037023 * data.dx_copd
-    LP0 += -0.033829 * data.dx_depression
-    LP0 += -0.033830 * data.dx_htn_nephropathy
-    LP0 += -0.001805 * data.dx_hyperlipidemia
-    LP0 += -0.098539 * data.dx_hypertension
-    LP0 += -0.017282 * data.dx_hypotension
-    LP0 += 0.651778 * data.dx_mi
-    LP0 += -0.141956 * data.dx_obesity
-    LP0 += 0.116652 * data.dx_oth_dysrhythmia
-    LP0 += -0.068198 * data.dx_psychosis
-    LP0 += -0.073889 * data.dx_rheumatic_heart
-    LP0 += -0.035560 * data.dx_sleep_apnea
-    LP0 += -0.015657 * data.dx_stable_angina
-    LP0 += -0.163684 * data.dx_valve_disorder
+    LP0 += -0.005747 * (data.age or 0)
+    LP0 += 0.275032 * (data.dx_defibrillator or 0)
+    LP0 += 0.346289 * (data.hosp_chf or 0)
+    LP0 += 0.221748 * (data.rx_ace or 0)
+    LP0 += 0.166008 * (data.rx_antagonist or 0)
+    LP0 += 0.087257 * (data.rx_bblocker or 0)
+    LP0 += 0.163224 * (data.rx_digoxin or 0)
+    LP0 += 0.084251 * (data.rx_loop_diuretic or 0)
+    LP0 += 0.129225 * (data.rx_nitrates or 0)
+    LP0 += -0.160819 * (data.rx_thiazide or 0)
+    LP0 += -0.002267 * (data.dx_afib or 0)
+    LP0 += -0.165353 * (data.dx_anemia or 0)
+    LP0 += -0.040175 * (data.dx_cabg or 0)
+    LP0 += 1.415113 * (data.dx_cardiomyopathy or 0)
+    LP0 += -0.037023 * (data.dx_copd or 0)
+    LP0 += -0.033829 * (data.dx_depression or 0)
+    LP0 += -0.033830 * (data.dx_htn_nephropathy or 0)
+    LP0 += -0.001805 * (data.dx_hyperlipidemia or 0)
+    LP0 += -0.098539 * (data.dx_hypertension or 0)
+    LP0 += -0.017282 * (data.dx_hypotension or 0)
+    LP0 += 0.651778 * (data.dx_mi or 0)
+    LP0 += -0.141956 * (data.dx_obesity or 0)
+    LP0 += 0.116652 * (data.dx_oth_dysrhythmia or 0)
+    LP0 += -0.068198 * (data.dx_psychosis or 0)
+    LP0 += -0.073889 * (data.dx_rheumatic_heart or 0)
+    LP0 += -0.035560 * (data.dx_sleep_apnea or 0)
+    LP0 += -0.015657 * (data.dx_stable_angina or 0)
+    LP0 += -0.163684 * (data.dx_valve_disorder or 0)
 
     # Add coefficient for heart failure type
     hf_type_mapping = {
@@ -89,7 +89,7 @@ def calculate_probability(data: ProbabilityInput) -> ProbabilityOutput:
         "Left": 0.766415,
         "Unspecified": -0.577221
     }
-    LP0 += hf_type_mapping[data.hf_type]
+    LP0 += hf_type_mapping.get(data.hf_type, 0)
 
     # Logistic function to calculate probability
     probability = 1 / (1 + np.exp(-LP0))
